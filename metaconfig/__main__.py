92c6ee50e40900b5081b9694b19b05e8c230c8bc
Danila Matyshev <gribnoysalatik@gmail.com>
2021-08-02 18:36:50 +0700

    new example

--- metaconfig/__main__.py
+++ metaconfig/__main__.py
@@ -1,36 +1,69 @@
-# demo usage
-import json
-from metaconfig.types import StrField
-from metaconfig import IntField, Fieldset, ConfigRoot, ListField
+# Импортируем необходимые классы из модуля
+from metaconfig import IntField, Fieldset, ConfigRoot, ListField, StrField, BoolField, JsonFileConfigIO
 
+
+# Создадим набор полей, содержащий поля хост и порт
 class HostPort(Fieldset):
-    host = StrField(label='Хост', default='0.0.0.0')
+    # Определим поле host как StrField с названием "Хост", подсказкой "IP адрес" и значеним по-умолчанию 0.0.0.0
+    host = StrField(label='Хост', default='0.0.0.0', hint="IP адрес")
     port = IntField(label='Порт', default=11000)
 
-class ABSum(Fieldset):
-    a:int = IntField(label='Значение А', default=1)
-    b:int = IntField(label='Значение Б', default=2)
-    hosts = ListField(
-                HostPort(label='Кассовый сервер'), 
-                default=[{'host': '192.168.1.100', 'port':15151}], 
-                label='Кассовые сервера'
-        )
+
+# Создадим другой набор полей
+class Server(Fieldset):
+    location = StrField(label="Местонахождение сервера", default='', hint="Страна")
+    # Используем определенный ранее набор полей с дополнительным пояснением
+    connection_data = HostPort(label='Данные для подключения')
 
 
+# Создадим корневой узел конфигурации 
 class Config(ConfigRoot):
-    sums = ABSum(label= 'Some A and B')
-    values = ListField(
-                IntField(label="Some integer", default=0), 
-                default=[3 , 5, '11'], 
-                label='Список чисел'
-            ) 
+    __io_class__ = JsonFileConfigIO('proxy.settings')
 
+    # Объявим список серверов
+    proxy_pool = ListField(
+            Server(
+                label="Прокси-сервер", 
+                default={
+                    'location': 'USA',
+                    'connection_data': {
+                        'host': 'proxy.google.com',
+                        'port': '3128'
+                    }
+                }
+            ),
+            label='Пул прокси-серверов',
+            default=[]
+        )
+    
+    # и адрес собственного сервера
+    home = Server(
+                label='Домашний сервер', 
+                default={
+                    'location': 'Russia', 
+                    'connection_data': {
+                        'host': 'home.server.com', 
+                        'port': 4123
+                    }
+                }
+            )
+
+    # а так же параметр, разрешающий отключить использование прокси
+    use_proxy = BoolField(label='Использовать прокси', default=True)
+    
+
+# Проинициализируем получившуюся конфигурацию
 config = Config()
-config.update({"sums":{"hosts":[{"port": "6066"}]}, "values": [2]})
 
-print(id(config))
-print(id(Config()))
-print(id(Config()))
+# Получим из нее конкретное значение (автокомплиты нам помогут)
+print(config.home.location)
+# Получим все данные из конфигурации
+print(config.as_dataset())
+# Получим метаданные о конфигурации
+print(config.as_metadata())
+# Точечно обновим один из параметров
+config.update({'use_proxy': False})
+
 
 # print(config.as_dataset())
 # print(json.dumps(config.as_dataset(), indent=2,ensure_ascii=False))
