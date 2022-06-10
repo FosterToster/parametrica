a63f095f9574e767cc5e63537977494b53e77e24
Danila Matyshev <gribnoysalatik@gmail.com>
2022-06-10 13:55:18 +0700

    yaml comments support

--- metaconfig/io.py
+++ metaconfig/io.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod, ABC
 import json
+import re
+
 
 class ConfigIOInterface(ABC):
 
@@ -76,20 +78,58 @@ class VirtualJsonFileConfigIO(JsonFileConfigIO, VirtualFile):
 
 
 class YAMLFileConfigIO(FileConfigIOInterface):
-    def __init__(self, filename: str) -> None:
+    def __init__(self, filename: str,*, export_comments: bool = True) -> None:
         super().__init__(filename)
+        self.export_comments = export_comments
         from importlib import import_module
         try:
             self.yaml = import_module('yaml')
         except ModuleNotFoundError as e:
             raise ImportError('Package "pyyaml" need to be installed.') from e
+
+    
+
+    def make_comments(self, fieldset: '_FieldRW', dataset:dict, resultstr: str, parent_fieldset: str = '', indent:int = 0) -> str:
+        for field_name in dataset.keys():
+            field = fieldset.__get_field__(field_name)
+                        
+            if field.__label__ == '' and field.__hint__ == '':
+                continue
+            if parent_fieldset == '':
+                search = re.compile(r'\A.*{}({})\:'.format(indent*" ", field_name), re.RegexFlag.S)
+            else:
+                search = re.compile(r'\A.*{}{}\:.*?{}({})\:'.format((indent-2)*" ", parent_fieldset, indent*" ", field_name), re.RegexFlag.S)
+            match = search.match(resultstr)
+            if match:
+                if len(match.regs) > 1:
+                    pos = match.regs[1][0]
+                    if field.__is_primitive_type__():
+                        comment = ''
+                        comment += field.__label__.replace("\n"," ").replace("\r", " ") + " " if field.__label__ else ''
+                        comment += field.__hint__.replace("\n"," ").replace("\r", " ") + " " if field.__hint__ else ''
+                        comment += f'({field.__generic_type__().__name__})'
+                        # comment += f' -> {field.__rule__}' if field.__rule__ else ''
+                        resultstr = resultstr[:pos]+f'# {comment}\n'+" "*indent+resultstr[pos:]
+                    else:
+                        comment = ''
+                        comment += field.__label__.replace("\n"," ").replace("\r", " ") + " " if field.__label__ else ''
+                        comment += field.__hint__.replace("\n"," ").replace("\r", " ") + " " if field.__hint__ else ''
+                        resultstr = resultstr[:pos]+f'# {comment}\n'+" "*indent+resultstr[pos:]
+            if not field.__is_iterable_type__() and not field.__is_primitive_type__():
+                resultstr = self.make_comments(field.__get__(fieldset, fieldset.__class__), dataset.get(field_name), resultstr, field_name, indent+2)
+
+        return resultstr
             
     def serialize(self, dataset: dict) -> str:
-        return self.yaml.dump(dataset, Dumper=self.yaml.Dumper)
+        resultstr = self.yaml.dump(dataset, sort_keys=False, default_flow_style=False, allow_unicode=True, Dumper=self.yaml.Dumper)
+        return self.make_comments(self.parent, dataset, resultstr) if self.export_comments else resultstr
 
     def parse(self, data: str) -> dict:
-        return self.yaml.load(data, Loader=self.yaml.Loader)
+        return self.yaml.load(data, Loader=self.yaml.Loader) or {}
 
 
 class VirtualYAMLFileConfigIO(YAMLFileConfigIO, VirtualFile):
     pass
+
+from .abc.fieldset import ABCMetaconfig
+from .abc.fieldset import _FieldRW
